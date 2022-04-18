b0c30b4d065dea1df95a670802cf49a6478ff81a
MateerialRepo <belloolaluqman@gmail.com>
2022-04-18 19:27:26 +0100

    Created Student class

---
+++ src/main/java/Student.java
@@ -0,0 +1,40 @@
+
+
+import java.util.List;
+
+public class Student {
+    private String stdName;
+    private List<String> lessonBookings;
+
+    public Student(String stdName, List<String> lessonBookings){
+        this.stdName = stdName;
+        this.lessonBookings = lessonBookings;
+    }
+
+    public void setLessonBookings(List<String> lessonBookings) {
+        this.lessonBookings = lessonBookings;
+    }
+
+    public void setStdName(String stdName) {
+        this.stdName = stdName;
+    }
+
+    public List<String> getLessonBookings() {
+        return lessonBookings;
+    }
+
+    public String getStdName() {
+        return stdName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "stdName='" + stdName + '\'' +
+                ", lessonBookings=" + lessonBookings +
+                '}';
+    }
+
+
+
+}
